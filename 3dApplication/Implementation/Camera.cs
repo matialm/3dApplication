002f132b48c954339780a85f4830313be4ad69ab
malmad <matias.almad@agea.com.ar>
2016-09-13 15:09:50 -0300

    Removed unused usings

--- 3dApplication/Implementation/Camera.cs
+++ 3dApplication/Implementation/Camera.cs
@@ -1,10 +1,6 @@
 ﻿using SharpDX;
 using SharpDX.DirectInput;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _3dApplication
 {
